955698a8e5742a0258c3d442ab70bef98147b41c
KIKIWU <wuqiweb@gmail.com>
2016-05-30 16:46:01 +0800

    增加发布帖子

--- src/app/home/classify/homeClassify.controller.js
+++ src/app/home/classify/homeClassify.controller.js
@@ -27,6 +27,7 @@
         vm.page = 1;
         //获取文章数据
         vm.getArticleByKind = getArticleByKind;
+        vm.publish = publish;
         
         
         activate();
@@ -40,6 +41,22 @@
             vm.getArticleByKind(vm.kindId, vm.page);
         }
 
+		function publish() {
+			var params = {};
+			params.title = vm.newArticle.title;
+			params.content = vm.newArticle.content;
+			params.id = vm.kindId;
+			homeApiService.user(null, params, function(result){
+
+                if(result.code == 200){
+                    var tips = {
+                        title: '发布成功',
+                        message: result.msg
+                    };
+                    dialogService.alert(tips);  
+                }
+            });
+		}
         function getArticleKinds() {
             var params = {};
             homeApiService.getArticleKinds(params, function(result){
@@ -49,26 +66,26 @@
                     vm.Kinds = kinds;            
                 }
             });
-            // var kinds = [
-            // 	{
-	           //      title: '社区活动',
-	           //      id: "7"
+            var kinds = [
+            	{
+	                title: '社区活动',
+	                id: "7"
 	        
-	           //  },{
-	           //      title: '考研专区',
-	           //      id: "2100"
+	            },{
+	                title: '考研专区',
+	                id: "2100"
 	               
-	           //  },{
-	           //      title: '兼职招聘',
-	           //      id: "387"
+	            },{
+	                title: '兼职招聘',
+	                id: "387"
 	               
-	           //  },{
-	           //      title: '校园专栏',
-	           //      id: "786"
+	            },{
+	                title: '校园专栏',
+	                id: "786"
 	                
-            // 	}
-            // ];
-            // vm.Kinds = kinds; 
+            	}
+            ];
+            vm.Kinds = kinds; 
         }
 
         function getArticleByKind(id, page) {
@@ -88,97 +105,97 @@
                     };               
                 }
             });
-     //        vm.articles = [
-	    //         {
-	    //         	   id: 3283,
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活',
-					// agree_count: 22
-	    //         },
-	    //         {
-	    //         	id: 1283,
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活',
-					// agree_count: 22
-	    //         },
-	    //         {
-	    //         	id: 1111,
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '林心如霍建华公开恋情',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活',
-					// agree_count: 38
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活',
-					// agree_count: 22
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '林心如霍建华公开恋情',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         },
-	    //         {
-					// tittle: '这是一个测试数据的文章标题',
-					// content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
-					// author: 'kikiwu',
-					// create_time: '2016年5月17日- 19:00',
-					// kinName: '校园生活'
-	    //         }
-     //        ];
-     //        var totalItems = 88;
+            vm.articles = [
+	            {
+	            	   id: 3283,
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活',
+					agree_count: 22
+	            },
+	            {
+	            	id: 1283,
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活',
+					agree_count: 22
+	            },
+	            {
+	            	id: 1111,
+					tittle: '这是一个测试数据的文章标题',
+					content: '林心如霍建华公开恋情',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活',
+					agree_count: 38
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活',
+					agree_count: 22
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '林心如霍建华公开恋情',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            },
+	            {
+					tittle: '这是一个测试数据的文章标题',
+					content: '5月12日晚8点，方励开始在自己的微博上直播与《百鸟朝凤》宣发相关的辛酸和感悟',
+					author: 'kikiwu',
+					create_time: '2016年5月17日- 19:00',
+					kinName: '校园生活'
+	            }
+            ];
+            var totalItems = 88;
 
-     //        vm.pager = {
-     //            totalItems: totalItems
-     //        };  
+            vm.pager = {
+                totalItems: totalItems
+            };  
         }
         function cheacklogin() {
             // var copy = angular.copy(vm.user);
@@ -200,6 +217,7 @@
         function _initialization() {
         	vm.isCollapsed = {
         	};
+        	vm.newArticle= {};
         }
     }
 })();
